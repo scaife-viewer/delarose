e45e2f527b1f205c5de14f8defffa8f33e0beb60
Jacob Wegner <jwegner@eldarion.com>
2019-10-21 13:35:04 -0500

    add basic pagination to UI

--- src/reader/components/ReaderPage.vue
+++ src/reader/components/ReaderPage.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="page">
+    <div v-if="previousPage" class="page-ref">PREVIOUS:
+      <!-- eslint-disable-next-line max-len -->
+      <router-link :to="{name: 'reader', query: { urn: previousPage.urn }}">{{ previousPage.pageRef }}</router-link>
+    </div>
     <div class="page-ref">PAGE: {{ page.identifier }}</div>
+    <div v-if="nextPage" class="page-ref">NEXT:
+      <!-- eslint-disable-next-line max-len -->
+      <router-link :to="{name: 'reader', query: { urn: nextPage.urn }}">{{ nextPage.pageRef }}</router-link>
+    </div>
     <div class="columns">
         <ReaderColumn
             v-for="column in page.columns.edges"
@@ -17,6 +25,29 @@ import ReaderColumn from './ReaderColumn.vue';
 export default {
   props: ['page'],
   components: { ReaderColumn },
+  methods: {
+    getPageSibling(offset) {
+      const pageRef = this.$store.state.pageIndex[this.page.idx + offset];
+      if (pageRef) {
+        return {
+          pageRef,
+          urn: `${this.version}:${pageRef}`,
+        };
+      }
+      return undefined;
+    },
+  },
+  computed: {
+    version() {
+      return this.$route.query.urn.split(':').slice(0, -1).join(':');
+    },
+    previousPage() {
+      return this.getPageSibling(-1);
+    },
+    nextPage() {
+      return this.getPageSibling(1);
+    },
+  },
 };
 </script>
 
