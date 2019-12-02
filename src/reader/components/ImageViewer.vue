245719a43e265d31d85db6bbb79e45154912a302
Jacob Wegner <jwegner@eldarion.com>
2019-12-02 12:44:10 -0600

    first pass at toggling between reader and image viewer

---
+++ src/reader/components/ImageViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="image-viewer">
+    <ImagePage
+      v-for="page in passageText"
+      v-bind:key="page.idx"
+      :page="page"
+    />
+  </div>
+</template>
+
+<script>
+import ImagePage from './ImagePage.vue';
+
+export default {
+  props: ['passage-text'],
+  components: { ImagePage },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import App from './App.vue';
+import { IMAGE_VIEWER_ROUTE } from './constants';
 
 Vue.use(Router);
 
 const routes = [
   { path: '/', redirect: 'reader' },
   { path: '/reader', component: App, name: 'reader' },
+  { path: '/image-viewer', component: App, name: IMAGE_VIEWER_ROUTE },
 ];
 
 const router = new Router({
